ecd57f26f4567ff3eed23df056eb59160ae539bd
OutsideLunch <ghrjf94@gmail.com>
2021-01-08 01:29:52 +0900

    문제 2) 사칙연산 프로그램 예외 던지기 (Calculator , CalculatorMain).

---
+++ src/ch10/InputMismatchException.java
@@ -0,0 +1,14 @@
+package ch10;
+
+public class InputMismatchException extends Exception {
+
+		
+		public InputMismatchException() {
+			
+		}
+		
+		public InputMismatchException(String message) {
+			super(message);
+		}
+	}
+
